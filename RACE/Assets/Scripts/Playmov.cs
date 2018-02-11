ddabede676a01e3015c684dcf1eaafa900d853b7
Matthew Stageman <mattstageman@gmail.com>
2018-02-11 18:03:32 +0000

    Working wheel
    Added a wheel that can go back and forwards.

--- RACE/Assets/Scripts/Playmov.cs
+++ RACE/Assets/Scripts/Playmov.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Playmov : MonoBehaviour {
+public class Playmov : MonoBehaviour
+{
+    public float motorMovement;
+    public WheelCollider wheel_;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        transform.Rotate(Vector3.left * 100 * Time.deltaTime);
+        float v = Input.GetAxis("Horizontal") * motorMovement;
+
+        wheel_.motorTorque = v;
 	}
 }
