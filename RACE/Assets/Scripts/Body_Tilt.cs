f13fe76bcbe0a64d2366567d90c319825e8a039c
Matthew Stageman <mattstageman@gmail.com>
2018-02-11 18:36:13 +0000

    Tilt script
    Added a tilt script. the angle at which the body tilts needs to be connected to the players torque.

---
+++ RACE/Assets/Scripts/Body_Tilt.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Body_Tilt : MonoBehaviour
+{
+
+    private float smooth = 2.0F;
+    private float tiltAngle = 30.0F;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        float tiltAroundX = Input.GetAxis("Horizontal") * tiltAngle;
+        Quaternion target = Quaternion.Euler(0, -90, -tiltAroundX);
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+    }
+}
---
+++ RACE/Assets/Scripts/Wheel_Rotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheel_Rotation : MonoBehaviour
+{
+    public float motorMovement;
+    public WheelCollider wheel_;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float v = motorMovement;
+
+        wheel_.motorTorque = v;
+    }
+}
