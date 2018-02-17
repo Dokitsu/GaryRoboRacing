64b19cbc1a6f4e6cd080517686cc6cb40eaf20db
Dokitsu <projectfuzzle@gmail.com>
2018-02-17 00:09:04 +0000

    Jemping
    added jumping and a check to see if the player is grounded

--- RACE/Assets/Scripts/Body_Tilt.cs
+++ RACE/Assets/Scripts/Body_Tilt.cs
@@ -12,7 +12,6 @@ public class Body_Tilt : MonoBehaviour
 
     private void Start()
     {
-        
     }
 
     private void Update()
@@ -26,5 +25,6 @@ public class Body_Tilt : MonoBehaviour
         {
             bodyAngle = bodyAngle + 360;
         }
+
     }
 }
--- RACE/Assets/Scripts/Wheel_Rotation.cs
+++ RACE/Assets/Scripts/Wheel_Rotation.cs
@@ -19,7 +19,11 @@ public class Wheel_Rotation : MonoBehaviour
     private float currentSpeed;
     private float maxSpeed = 150;
     private float minSpeed = 50;
-    
+
+    public CapsuleCollider col;
+
+    public LayerMask groundLayer;
+
     // Use this for initialization
     void Start()
     {
@@ -33,26 +37,26 @@ public class Wheel_Rotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        angleSpeed = Body.GetComponent<Body_Tilt>().bodyAngle; 
+        angleSpeed = Body.GetComponent<Body_Tilt>().bodyAngle;
 
         float v = angleSpeed;
 
-        if(currentSpeed < maxSpeed && currentSpeed > minSpeed)
+        if (currentSpeed < maxSpeed && currentSpeed > minSpeed)
         {
             wheel_.motorTorque = v;
         }
-        if(currentSpeed > maxSpeed)
+        if (currentSpeed > maxSpeed)
         {
             wheel_.motorTorque = 0;
             //Debug.Log("Max");
         }
-        if(currentSpeed < minSpeed)
+        if (currentSpeed < minSpeed)
         {
             wheel_.motorTorque = 10;
             //Debug.Log("Min");
         }
 
-        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             wheel_.brakeTorque = breakForce;
         }
@@ -60,5 +64,20 @@ public class Wheel_Rotation : MonoBehaviour
         {
             wheel_.brakeTorque = 0;
         }
+
+
+
+        if (Grounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            bodyVelocity.AddForce(Vector3.up * 10, ForceMode.Impulse);
+        }
     }
+
+
+    private bool Grounded()
+    {
+        return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x, col.bounds.min.y, col.bounds.center.z),col.radius,groundLayer);
+    }
+
+    
 }
