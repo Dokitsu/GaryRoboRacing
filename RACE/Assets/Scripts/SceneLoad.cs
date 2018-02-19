9e6aff210ee49844e7efa5aaf52e28401fbcedca
dufas3 <skirma22@gmail.com>
2018-02-19 21:49:32 +0000

    main menu added
    title

---
+++ RACE/Assets/Scripts/SceneLoad.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoad : MonoBehaviour {
+
+    [SerializeField]
+    string _sceneName;
+    public void Loadscene()
+    {
+        SceneManager.LoadScene(_sceneName);
+    }
+}
