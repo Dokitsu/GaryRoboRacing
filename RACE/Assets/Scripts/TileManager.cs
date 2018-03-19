4996d8fa119aa08b097f21f705d6f8a5f908d23f
Matthew Stageman <mattstageman@gmail.com>
2018-03-19 23:17:26 +0000

    added new asset pack and other changes
    asset pack has essential assets and skybox for background. the tile spawning script now spawns the basic tile prefab which need the mesh added once dan uploads his model.

---
+++ RACE/Assets/Scripts/TileManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    // array of prefabs need spawning
+    public GameObject[] TilePrefabs;
+
+    //variables for spawning
+    private Transform playerTransform;
+    private float spawnZ = 10.0f;
+    private float tileLength = 20.0f;
+    private int numOfTiles = 6;
+
+    //spawning random tiles
+    private int lastPrefabIndex = 0;
+    private List<GameObject> activeTiles;
+
+    // Use this for initialization
+    void Start()
+    {
+        activeTiles = new List<GameObject>();
+        playerTransform = GameObject.FindGameObjectWithTag("Campos").transform;
+
+        for (int i = 0; i < numOfTiles; i++)
+        {
+            SpawnTile();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerTransform.position.z > (spawnZ + 60 - numOfTiles * tileLength))
+        {
+            SpawnTile();
+            DeleteTile();
+        }
+    }
+    //method that spawns the tiles
+    private void SpawnTile(int prefabIndex = -1)
+    {
+        GameObject go;
+        go = Instantiate(TilePrefabs[RandomPrefabIndex()]) as GameObject;
+        go.transform.SetParent(transform);
+        go.transform.position = Vector3.forward * spawnZ;
+        spawnZ += tileLength;
+        activeTiles.Add(go);
+    }
+    //method that deletes tiles
+    private void DeleteTile()
+    {
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
+    }
+    //method that picks randomly one of the prefabs in the prefabs array
+    private int RandomPrefabIndex()
+    {
+        if (TilePrefabs.Length <= 1)
+            return 0;
+
+        int randomIndex = lastPrefabIndex;
+        while (randomIndex == lastPrefabIndex)
+        {
+            randomIndex = Random.Range(0, TilePrefabs.Length);
+        }
+
+        lastPrefabIndex = randomIndex;
+        return randomIndex;
+    }
+}
