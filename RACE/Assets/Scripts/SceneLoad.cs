63cc675f6de488a99198296fd93f5cd1bf8d9371
Matthew Stageman <mattstageman@gmail.com>
2018-03-23 21:00:53 +0000

    don't destroy on load for background music

--- RACE/Assets/Scripts/SceneLoad.cs
+++ RACE/Assets/Scripts/SceneLoad.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneLoad : MonoBehaviour {
-
+public class SceneLoad : MonoBehaviour
+{
     public void Lobby1()
     {
         SceneManager.LoadScene(1);
